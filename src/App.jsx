0567bbe1bc1ae08fc0feeaa1fb5721bb641d4590
Thomas Mueller <thomasmueller87@t-online.de>
2021-12-13 15:40:15 +0100

    Funtionierende Felder, Auslagerung noch offen

--- src/App.jsx
+++ src/App.jsx
@@ -3,88 +3,145 @@ import "./App.css";
 import styled from "styled-components";
 
 function App() {
-  const [product, setProduct] = useState({
-    name: "Schnapps",
-    price: 50,
+  const productDetails = {
+    name: "Glühwein",
+    price: "5",
     isVegan: true,
     category: "",
-    packageSize: "",
-    contactEmail: "",
-  });
+    packageSize: "S",
+    email: "test@testkunde.de",
+  };
+
+  const [product, setProduct] = useState(productDetails);
+
   const handleChange = (event) => {
     let inputValue = event.target.value;
-
     if (event.target.type === "checkbox") {
       inputValue = event.target.checked;
     }
+    if (event.target.name === "price") {
+      inputValue = Number(inputValue);
+    }
     setProduct({
-      //alle bestehenden Probs behalten
       ...product,
-      //neue Daten
       [event.target.name]: inputValue,
     });
   };
-
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(product);
   };
-  console.log(product);
+  const handleReset = () => {
+    setProduct(productDetails);
+  };
   return (
-    <div className="App">
-      <Container>
-        <h2>Add a new product</h2>
-        <Form onSubmit={handleSubmit}>
-          <label htmlFor="name">Product name</label>
+    <Container className="App">
+      <h2>Weihnachts-Store</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Productname: </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={handleChange}
+          value={product.name}
+        />
+        <label htmlFor="price">Price: </label>
+        <input
+          type="text"
+          name="price"
+          id="price"
+          onChange={handleChange}
+          value={product.price}
+        />
+        <label>
+          vegan:
           <input
-            type="text"
-            name="name"
-            id="name"
+            type="checkbox"
+            name="isVegan"
+            id="isVegan"
             onChange={handleChange}
-            value={product.name}
+            checked={product.isVegan}
           />
-
-          <label htmlFor="price">Price (in EUR)</label>
+        </label>
+        <label htmlFor="category">Category: </label>
+        <select
+          name="category"
+          id="category"
+          onChange={handleChange}
+          value={product.category}
+        >
+          <option value="" disabled>
+            ...choose a category
+          </option>
+          <option value="drinks">Drinks</option>
+          <option value="food">Food</option>
+          <option value="tools">Tools</option>
+        </select>
+        <label htmlFor="packageSize" className="package">
+          Package size:
+          <label htmlFor="packageSize">S</label>
           <input
-            type="text"
-            id="price"
-            name="price"
+            type="radio"
+            name="packageSize"
+            id="packageSize"
+            value="S"
             onChange={handleChange}
-            value={product.price}
+            checked={product.packageSize === "S"}
           />
-
-          <label>
-            <input
-              type="checkbox"
-              name="isVegan"
-              id="isVegan"
-              onChange={handleChange}
-              checked={product.isVegan}
-            />{" "}
-            vegan
-          </label>
-
-          <button>Add Product</button>
-        </Form>
-      </Container>
-    </div>
+          <label htmlFor="packageSize">M</label>
+          <input
+            type="radio"
+            name="packageSize"
+            id="packageSize"
+            value="M"
+            onChange={handleChange}
+            checked={product.packageSize === "M"}
+          />
+          <label htmlFor="packageSize">L</label>
+          <input
+            type="radio"
+            name="packageSize"
+            id="packageSize"
+            value="L"
+            onChange={handleChange}
+            checked={product.packageSize === "L"}
+          />
+        </label>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          onChange={handleChange}
+          value={product.email}
+        />
+        <button>Add product</button>
+        <input type="reset" value="reset" onClick={handleReset} />
+      </form>
+    </Container>
   );
 }
-
+export default App;
 const Container = styled.div`
-  max-width: 24rem;
-  margin: 0 auto;
-`;
-
-const Form = styled.form`
+  max-width: 25rem;
+  margin: auto;
+  form {
+    padding: 2rem;
+  }
   label {
     display: block;
-    font-weight: bold;
+    margin-bottom: 10px;
   }
   input {
     padding: 0.25rem;
-    margin-bottom: 0.5rem;
+    margin-bottom: 1rem;
+  }
+  select {
+    margin-bottom: 1rem;
+  }
+  .package {
+    display: flex;
+    justify-content: space-around;
   }
 `;
-
-export default App;
